b9835df294c4006bd595637b53d824b9093ccfad
Mahil Anthony <mahilsamson2016@gmail.com>
2024-08-30 09:09:27 +0530

    Update EXP-11-080.cpp

--- EXP-11-080.cpp
+++ EXP-11-080.cpp
@@ -1,1 +1,26 @@
+//Experiment 11 - To study and implement classes and objects.
+//Name - Mahil Anthony
+//PRN -  23070123080
+//Batch -  ENTC B1
 
+//Program 1 (Defining a class.)
+#include<iostream>
+using namespace std;
+class Car
+{
+    public:
+    string make = "Mahindra";
+    string model = "XUV 700";
+    int year = 2024;
+};
+int main()
+{
+    Car myCar;
+    cout<<"Car Details: "<<myCar.make<<" " <<myCar.model<<" ("<<myCar.year<<")"<<endl;
+    return 0;
+}
+
+//Output
+/*
+Car Details: Mahindra XUV 700 (2024)
+*/
